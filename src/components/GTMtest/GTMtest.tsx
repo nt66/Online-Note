2a51bff58e064669872ef88ad6e3e1d2df265789
nt66 <tchualingfeng@gmail.com>
2025-06-23 20:20:26 +0900

    gtm test

---
+++ src/components/GTMtest/GTMtest.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./GTMtest.less";
+
+const GTMtest: React.FC = () => {
+  return (
+    <div className="gtm-test">
+      <h1>GTM Test </h1>
+      <div className="test-content">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/fMevmwbwVh8"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default GTMtest;
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Online-Note",
+  base: process.env.NODE_ENV === 'production' ? "/Online-Note/" : "/",
   plugins: [
     react(),
   ],
+  server: {
+    port: 5173,
+    host: true
+  }
 })
